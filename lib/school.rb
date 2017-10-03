bde912efa651ba68e0cd681059ed3327b4f80166
Kristen Peck <kpeck71@gmail.com>
2017-10-03 00:46:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,9 +12,9 @@ class School
   #   ROSTER << roster
   # end
 
-  #def add_student(grade,name)
-  #  unless @@roster include?(grade)
-    #roster[grade] << name
-  #end
+  def add_student(grade,name)
+   unless @@roster include?(grade)
+    roster[grade] << name
+  end
 
 end
