0a89a538478e56440c7b5c16e8746b13879fbaa6
Kristen Peck <kpeck71@gmail.com>
2017-10-03 01:08:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,11 +11,11 @@ class School
   def add_student(name, grade)
   #binding.pry
   @roster ||= roster[grade]
-  unless @roster.include?(grade)
+    unless @roster.include?(grade)
     #roster[grade] = []
 #    roster.map do |name, grade|
       roster[grade] << name
-  #  end
+    end
   end
 
 end
